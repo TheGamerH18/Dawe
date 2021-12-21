532d8506960383adf20de2375b24b4239ae6f8e6
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-12-21 22:49:33 +0100

    Add Logging in DbContext

--- Dawe/Data/DataContext.cs
+++ Dawe/Data/DataContext.cs
@@ -4,15 +4,24 @@ namespace Dawe.Data
 {
     public class DataContext : DbContext
     {
+        private readonly ILogger<DataContext> _logger;
+
         public DbSet<Models.Movies>? Movies { get; set; }
         public DbSet<Models.Show>? Shows { get; set; }
         public DbSet<Models.Episode>? Episodes { get; set;}
         public string DBPath { get; set; }
 
+        public DataContext(ILogger<DataContext> logger1)
+        {
+            _logger = logger1;
+
+            DBPath = System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Dawe.db");
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            DBPath = System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Dawe.db");
             optionsBuilder.UseSqlite($"Data source={DBPath}");
+            _logger.LogInformation($"Database Loaded from {DBPath}");
         }
     }
 }
