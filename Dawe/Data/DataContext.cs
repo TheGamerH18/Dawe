c79b8fa762ca21faabb6cb6412f518c6ee3882be
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-12-21 12:13:36 +0100

    Projektdateien hinzufügen.

---
+++ Dawe/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dawe.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Models.Movies>? Movies { get; set; }
+        public string DBPath { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DBPath = System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Dawe.db");
+            optionsBuilder.UseSqlite($"Data source={DBPath}");
+        }
+    }
+}
---
+++ Dawe/Models/Movies.cs
@@ -0,0 +1,19 @@
+﻿using SixLabors.ImageSharp;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Dawe.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int Id { get; set; }
+        public string MoviePath { get; set; } = string.Empty;
+        public byte[] Cover { get; set; } = null;
+        public string Name { get; set; } = string.Empty;
+
+        public List<string> Tags { get; } = new();
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
