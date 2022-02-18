4b82013c77b367f00addacad6633b2eafcf14bfe
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2022-02-18 09:07:25 +0100

    Adding Index

---
+++ Dawe/Controllers/FileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dawe.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
