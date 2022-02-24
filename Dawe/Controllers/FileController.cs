436644ea876e18b6a5fe573109794dab0c74fcc6
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2022-02-24 11:43:06 +0100

    Adding Create Category

--- Dawe/Controllers/FileController.cs
+++ Dawe/Controllers/FileController.cs
@@ -23,5 +23,35 @@ namespace Dawe.Controllers
             var files = _context.Files.ToList();
             return View(files);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CategoryCreateModel createModel)
+        {
+            FileCategory category = new() { Name = createModel.Text };
+            await _context.FileCategories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public class FileCreateModel
+        {
+            public string Name { get; set; }
+            public string Path { get; set; }
+        }
+
+        public class CategoryCreateModel
+        {
+            public string Text { get; set; }
+        }
     }
 }
