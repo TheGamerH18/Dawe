176ecaa65779c6004e70a26cc0aa50896ddb16c9
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2022-02-25 13:22:24 +0100

    Add DeleteFile to Helper

--- Dawe/Data/Helper.cs
+++ Dawe/Data/Helper.cs
@@ -13,12 +13,41 @@
         }
 
         // Create Full Path to File
-        public  static string GetPathAndFilename(string filename, string WebRootPath)
+        public static string GetPathAndFilename(string filename, string WebRootPath)
         {
             string subfolder = "uploads/";
             // return _hostingEnvironment.WebRootPath + "\\uploads\\" + filename;
             string path = Path.Combine(WebRootPath, subfolder);
             return Path.Combine(path, filename);
+        }
+
+        /// <summary>
+        /// Checks if a uploaded file exists
+        /// </summary>
+        /// <param name="filename">Name of file to be checked</param>
+        /// <param name="WebRootPath">WebRootPath from a IWebHostEnvironment</param>
+        /// <returns>True, if the file exists</returns>
+        public static bool FileExists(string filename, string WebRootPath)
+        {
+            string path = GetPathAndFilename(filename, WebRootPath);
+            return File.Exists(path);
+        }
+
+        /// <summary>
+        /// Deletes the specified uploaded File
+        /// </summary>
+        /// <param name="filename">Name of File to be deleted</param>
+        /// <param name="WebRootPath">WebRootPath from a IWebHostEnvironment</param>
+        /// <returns>True, the file is deleted successfully</returns>
+        public static bool DeleteFile(string filename, string WebRootPath)
+        {
+            if(FileExists(filename, WebRootPath))
+            {
+                var path = GetPathAndFilename(filename, WebRootPath);
+                File.Delete(path);
+                return true;
+            }
+            return false;
         }
     }
 
